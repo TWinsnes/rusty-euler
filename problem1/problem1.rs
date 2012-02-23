c4a76cd091e0f15999a76678d39a64970b0dd1ac
Thomas Winsnes <thomas@ideadrought.com>
2012-02-23 21:09:00 +1100

    Solved the first problem as well as updating the readme file

---
+++ problem1/problem1.rs
@@ -0,0 +1,35 @@
+use std;
+
+fn main()
+{
+	std::io::println(#fmt("Solution: %d",solution(3,5,1,1000)));
+}
+
+// my first solution : O(n)
+fn solution(x: int, y: int, from: int, to: int) -> int
+{
+	let sum = 0;
+	let counter = from;
+
+	while counter < to
+	{
+		if (counter % x == 0 || counter % y == 0)
+		{
+			sum += counter;
+		}
+
+		counter += 1;
+	}
+
+	ret sum;
+}
+
+// test for solution
+#[test]
+fn testSolution()
+{
+	let result: int = solution(3,5,1,10);
+
+	assert result == 23;
+
+}
